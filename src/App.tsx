c9ca0003d6af48bb6a29e93cb3a2b54dfeb1b151
Stephanie Gabrielle Pieri <stephaniegabriellepieri@192.168.1.106>
2026-06-19 17:02:46 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { useState, useEffect } from 'react';
-const BASE_URL = 'https://my-server.onrender.com';
+
+const BASE_URL = 'https://my-server-an5n.onrender.com';
+
+type Task = {
+  id: number;
+  title: string;
+};
+
 export default function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
+
   useEffect(() => {
     fetch(`${BASE_URL}/tasks`)
       .then(res => res.json())
       .then(data => setTasks(data));
   }, []);
+
   return (
     <ul>
       {tasks.map(task => (
